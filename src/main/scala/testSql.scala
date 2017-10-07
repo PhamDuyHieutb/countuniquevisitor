ce10a4b5cc0c5e76996053a9e4e2175a176595c2
PhamDuyHieutb <phamduyhieuit@gmail.com>
2017-10-07 11:45:37 +0700

    c

--- src/main/scala/testSql.scala
+++ src/main/scala/testSql.scala
@@ -28,28 +28,43 @@ object test{
     //    val spark = SparkSession.builder().master("local").appName("Log Query").enableHiveSupport().getOrCreate()
     val logdata = sqlContext.read.parquet("/data/Parquet/AdnLog/2017_10_02/*").repartition(10)
     logdata.registerTempTable("log")
-    val sqlClickResult= sqlContext.sql("select guid,domain,path,click_or_view from log ").filter(e => !(e.getString(1)!="kenh14.vn"))
+//    val sqlClickResult= sqlContext.sql("select guid,domain,path,click_or_view from log ").filter(e => !(e.getString(1)!="kenh14.vn"))
+//    val a = sqlClickResult.map(a =>  (a.getLong(0), a.getString(1)+""+a.getString(2),a.getBoolean(3)))
+//    val urlkenh14 = sqlContext.read.csv("kenh14.csv")
+//    val newNames = Seq("url", "label")
+//    val dfRenamed = urlkenh14.toDF(newNames: _*)
+//    val newNames_a = Seq("guid", "url","click_or_view")
+//    val dfRenamed_a = a.toDF(newNames_a: _*)
+//
+//
+//
+//
+//    val result = dfRenamed_a.join(dfRenamed,Seq("url"))
+//    val view = result.count()
+//    val filterresult =   result.filter(e => e.getString(2)!="0")
+//    val viewlabel_1 = filterresult.count()
+//
+//    print("unique visitor 2017_10_02 =================== " + filterresult.select("guid").distinct().count()+ "   co "+ viewlabel_1 +" view nhan 1 tren tong "+ view +" view")
+
+
+
+
+    val sqlClickResult = sqlContext.sql("select guid,domain,path,click_or_view from log ")
+    val sqlfilter = sqlClickResult.toDF().filter(sqlClickResult("domain").startsWith("kenh14.vn"))
     val a = sqlClickResult.map(a =>  (a.getLong(0), a.getString(1)+""+a.getString(2),a.getBoolean(3)))
-    val urlkenh14 = sqlContext.read.csv("kenh14.csv")
+    val urlkenh14 = sqlContext.read.format("csv").load("kenh14.csv")
     val newNames = Seq("url", "label")
     val dfRenamed = urlkenh14.toDF(newNames: _*)
     val newNames_a = Seq("guid", "url","click_or_view")
     val dfRenamed_a = a.toDF(newNames_a: _*)
 
 
-
-
     val result = dfRenamed_a.join(dfRenamed,Seq("url"))
-    val view = result.count()
-    val filterresult =   result.filter(e => e.getString(2)!="0")
-    val viewlabel_1 = filterresult.count()
+        val view = result.count()
+        val filterresult =   result.filter(result("label").contains("0") )
+        val viewlabel_1 = filterresult.count()
 
-    print("unique visitor 2017_10_02 =================== " + filterresult.select("guid").distinct().count()+ "   co "+ viewlabel_1 +" view nhan 1 tren tong "+ view +" view")
-
-
-
-
-
+        print("unique visitor 2017_10_02 =================== " + filterresult.select("guid").distinct().count()+ "   co "+ viewlabel_1 +" view nhan 1 tren tong "+ view +" view")
   }
 
 }
