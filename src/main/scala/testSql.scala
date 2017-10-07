20a80968c1217f9e35584b1a6c47eb205e64e8c5
PhamDuyHieutb <phamduyhieuit@gmail.com>
2017-10-07 10:59:18 +0700

    c

--- src/main/scala/testSql.scala
+++ src/main/scala/testSql.scala
@@ -1,7 +1,6 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.countDistinct
 
 
